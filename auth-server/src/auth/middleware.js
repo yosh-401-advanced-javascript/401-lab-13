59eeb08484932a53df2d5568c993dabf2ac214de
lilbonito <sauteboy@gmail.com>
2019-08-28 20:21:20 -0700

    first commit

---
+++ auth-server/src/auth/middleware.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const User = require('./users-model.js');
+
+module.exports = (req, res, next) => {
+  
+  try {
+    let [authType, authString] = req.headers.authorization.split(/\s+/);
+    
+    switch( authType.toLowerCase() ) {
+      case 'basic':
+        return _authBasic(authString);
+      case 'bearer':
+        return _authBearer(authString);
+      default: 
+        return _authError();
+    }
+  }
+  catch(e) {
+    next(e);
+  }
+
+  /**
+   *
+   * @param str
+   * @returns {Promise<T>}
+   * @private
+   */
+  function _authBasic(str) {
+    // str: am9objpqb2hubnk=
+    let base64Buffer = Buffer.from(str, 'base64'); // <Buffer 01 02 ...>
+    let bufferString = base64Buffer.toString();    // john:mysecret
+    let [username, password] = bufferString.split(':'); // john='john'; mysecret='mysecret']
+    let auth = {username,password}; // { username:'john', password:'mysecret' }
+    
+    return User.authenticateBasic(auth)
+      .then(user => _authenticate(user) )
+      .catch(next);
+  }
+
+  function _authBearer(authString) {
+    return User.authenticateToken(authString)
+        .then( user => _authenticate(user))
+        .catch(next);
+  }
+
+  function _authenticate(user) {
+    if(user) {
+      req.user = user;
+      req.token = user.generateToken();
+      next();
+    }
+    else {
+      _authError();
+    }
+  }
+  
+  function _authError() {
+    next('Invalid User ID/Password');
+  }
+  
+};
